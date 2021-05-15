d53813b1c28292b346efaa7035c0ee221e089345
Lnatt <dima.naryshkin43@gmail.com>
2021-05-16 00:17:55 +0300

    Добавьте файлы проекта.

---
+++ DataManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoBD_lab1
+{
+    public class DataManager
+    {
+        private IMongoDatabase _database;
+        public DataManager(string connectionString, string databaseName) // конструктор 
+        {
+            var mongoClient = new MongoClient(connectionString);
+            _database =  mongoClient.GetDatabase(databaseName);
+        }
+        public IEnumerable<BsonDocument> GetUsers() 
+        {
+            var collection = _database.GetCollection<BsonDocument>("users");
+            var docs = collection.Find(x => true);
+            return docs.ToEnumerable();
+        }
+        public IEnumerable<BsonDocument>GetUsersByName(string name) // ищем по имени
+        {
+            var collection = _database.GetCollection<BsonDocument>("users");
+            var users = collection.Find(x => x["name"].Equals(name));
+            return users.ToEnumerable();
+        }
+        public IEnumerable<User> GetUsersByNameTyped(string name)
+        {
+            var collection = _database.GetCollection<User>("users");
+            var users = collection.Find(x => x.Name.Equals(name));
+            return users.ToEnumerable();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace MongoBD_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataManager = new DataManager("mongodb://localhost:27017", "seminar");
+            //var users = dataManager.GetUsers();
+            var users = dataManager.GetUsersByName("c");
+            foreach ( var user in users)
+            {
+                Console.WriteLine(string.Join(',', user.Values));
+                /*if (user.Names.Contains("name")) 
+                { 
+                    Console.WriteLine(user["name"]); 
+                }*/
+            }
+            var typedUsers = dataManager.GetUsersByNameTyped("c");
+            foreach (var user in typedUsers)
+            {
+                Console.WriteLine(user.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
